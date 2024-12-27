b435c937ff64ad4f9afdb280030846318c6390ca
Ansh Malik <ansh.malik@delhidams.com>
2024-12-27 16:53:56 +0530

    Change Vite output directory to 'build'

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import  { useState, useContext } from 'react';
 import { contextApi } from '../../useContextApi/useContextApi';
 import './Contact.css';
 import { MdOutlineMail } from "react-icons/md";
